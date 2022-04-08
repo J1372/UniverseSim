8ac1adefbd42732a75315a294c00ba3b58243288
darkmatterturtlepasta <95717261+Xxthedarkmatterturtlepasta003xX@users.noreply.github.com>
2022-04-07 20:57:33 -0700

    Added basic private members to TextLine

--- Planets2/TextBox.h
+++ Planets2/TextBox.h
@@ -1,9 +1,29 @@
 #pragma once
 
 #include "UIElement.h"
+#include <string>
+#include <raylib.h>
+#include <functional>
 
 class TextBox : public UIElement
 {
+	std::string entered_text;
+
+	int font_size;
+	int text_length_pixels;
+	double width_padding = 0.1;
+
+	Rectangle rect;
+
+	Color background_color = GRAY;
+	Color edge_color = DARKGRAY;
+
+	bool editable = false;
+	static constexpr int edge_width = 10;
+
+	std::function<void(std::string& text)> callback = nullptr;
+
+public:
 
 };
 
