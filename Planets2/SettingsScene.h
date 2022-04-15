7f97579d67d9694925bd985e03cf443c1c78d477
darkmatterturtlepasta <95717261+Xxthedarkmatterturtlepasta003xX@users.noreply.github.com>
2022-04-15 00:07:15 -0700

    Added variables to SettingsScene for easier changing of gui element positions.

--- Planets2/SettingsScene.h
+++ Planets2/SettingsScene.h
@@ -7,14 +7,23 @@
 
 class SettingsScene : public GuiScene
 {
-	Button start_button = gui.add<Button>(&start_button, "Start", 500, 700);
-	Button exit_button = gui.add<Button>(&exit_button, "Exit", 600, 700);
 
-	TextBox num_planets_input = gui.add<TextBox>(&num_planets_input, 300, 400, 500);
-	TextBox num_systems_input = gui.add<TextBox>(&num_systems_input, 300, 500, 500);
+	static constexpr int BUTTON_Y = 700;
 
-	Label num_planets_label = gui.add<Label>(&num_planets_label, "Num planets", 900, 420, 12);
-	Label num_systems_label = gui.add<Label>(&num_systems_label, "Num systems", 900, 520, 12);
+	Button start_button = gui.add<Button>(&start_button, "Start", 500, BUTTON_Y);
+	Button exit_button = gui.add<Button>(&exit_button, "Exit", 600, BUTTON_Y);
+
+	static constexpr int TEXTBOX_START_X = 300;
+	static constexpr int TEXTBOX_WIDTH = 500;
+
+
+	TextBox num_planets_input = gui.add<TextBox>(&num_planets_input, TEXTBOX_START_X, 400, TEXTBOX_WIDTH);
+	TextBox num_systems_input = gui.add<TextBox>(&num_systems_input, TEXTBOX_START_X, 500, TEXTBOX_WIDTH);
+
+	static constexpr int LABEL_X = TEXTBOX_START_X + TEXTBOX_WIDTH + 100;
+
+	Label num_planets_label = gui.add<Label>(&num_planets_label, "Num planets", LABEL_X, 420, 12);
+	Label num_systems_label = gui.add<Label>(&num_systems_label, "Num systems", LABEL_X, 520, 12);
 
 public:
 	SettingsScene(int width, int height);
