7388739d5b0a90a56e491f0e23f09dda8e0f0830
darkmatterturtlepasta <95717261+Xxthedarkmatterturtlepasta003xX@users.noreply.github.com>
2022-05-12 18:45:19 -0700

    Created a wrapper camera around Raylib's Camera2D struct that can be easily used in different camera states.

---
+++ Planets2/AdvCamera.cpp
@@ -0,0 +1,137 @@
+#include "AdvCamera.h"
+#include <utility>
+
+
+void AdvCamera::recalculate_speed_target()
+{
+	target_speed = 5 * target_speed_multiplier / camera.zoom;
+}
+
+AdvCamera::AdvCamera(Vector2& offset, Vector2& target) : target_speed_multiplier(8), offset_bounds{ 0, 0, static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight()) }
+{
+	camera.offset = offset;
+	camera.target = target;
+	camera.zoom = 1.0f;
+	recalculate_speed_target();
+}
+
+AdvCamera::AdvCamera(Vector2& offset, Vector2& target, int target_speed_multiplier) : target_speed_multiplier(target_speed_multiplier), offset_bounds{ 0, 0, static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight()) }
+{
+	camera.offset = offset;
+	camera.target = target;
+	camera.zoom = 1.0f;
+	recalculate_speed_target();
+}
+
+AdvCamera::AdvCamera(Vector2&& offset, Vector2&& target) : target_speed_multiplier(8), offset_bounds{ 0, 0, static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight()) }
+{
+	camera.offset = std::move(offset);
+	camera.target = std::move(target);
+	camera.zoom = 1.0f;
+	recalculate_speed_target();
+}
+
+AdvCamera::AdvCamera(Vector2&& offset, Vector2&& target, int target_speed_multiplier) : target_speed_multiplier(target_speed_multiplier), offset_bounds{ 0, 0, static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight()) }
+{
+	camera.offset = std::move(offset);
+	camera.target = std::move(target);
+	camera.zoom = 1.0f;
+	recalculate_speed_target();
+}
+
+void AdvCamera::zoom_in()
+{
+	camera.zoom *= 2;
+	recalculate_speed_target();
+}
+
+void AdvCamera::zoom_out()
+{ 
+	camera.zoom /= 2;
+	recalculate_speed_target();
+}
+
+void AdvCamera::increase_speed_target()
+{
+	target_speed_multiplier++;
+	recalculate_speed_target();
+}
+
+void AdvCamera::decrease_speed_target()
+{
+	if (target_speed_multiplier > 1) {
+		target_speed_multiplier--;
+		recalculate_speed_target();
+	}
+}
+
+void AdvCamera::increase_speed_offset()
+{
+	offset_speed += OFFSET_INCREMENT;
+}
+
+void AdvCamera::decrease_speed_offset()
+{
+	// Prevent movement from going dropping to zero or going negative.
+	if (offset_speed > OFFSET_INCREMENT) {
+		offset_speed -= OFFSET_INCREMENT;
+	}
+}
+
+void AdvCamera::move_target(Direction dir)
+{
+	switch (dir) {
+	case Direction::LEFT:
+		camera.target.x -= target_speed;
+		break;
+	case Direction::UP:
+		camera.target.y -= target_speed;
+		break;
+	case Direction::RIGHT:
+		camera.target.x += target_speed;
+		break;
+	case Direction::DOWN:
+		camera.target.y += target_speed;
+		break;
+	}
+}
+
+void AdvCamera::move_offset(Direction dir)
+{
+	switch (dir) {
+	case Direction::LEFT:
+		camera.offset.x = std::max(camera.offset.x - offset_speed, offset_bounds.x);
+		break;
+	case Direction::UP:
+		camera.offset.y = std::max(camera.offset.y - offset_speed, offset_bounds.y);
+		break;
+	case Direction::RIGHT:
+		camera.offset.x = std::min(camera.offset.x + offset_speed, offset_bounds.width);
+		break;
+	case Direction::DOWN:
+		camera.offset.y = std::min(camera.offset.y + offset_speed, offset_bounds.height);
+		break;
+	}
+}
+
+void AdvCamera::move_target(Vector2 vec)
+{
+	camera.target.x += vec.x;
+	camera.target.y += vec.y;
+}
+
+void AdvCamera::move_offset(Vector2 vec)
+{
+	camera.offset.x += vec.x;
+	camera.offset.y += vec.y;
+}
+
+void AdvCamera::set_target(Vector2 vec)
+{
+	camera.target = vec;
+}
+
+void AdvCamera::set_offset(Vector2 vec)
+{
+	camera.offset = vec;
+}
---
+++ Planets2/AdvCamera.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "raylib.h"
+
+enum class Direction {
+	LEFT,
+	UP,
+	RIGHT,
+	DOWN
+};
+
+// A wrapper around the Raylib Camera2D struct. Adding functionality for zooming and setting target and offset velocity.
+class AdvCamera
+{
+	/*
+	Camera2D has two separate Vector2's, offset and target.
+
+	The target is the position in the world that the camera is focused around.
+	It uses world coordinates.
+
+	The offset is the position on screen that the target should be shown at.
+	(0,0) = top left
+	(screen_width, screen_height) = bottom right
+	It uses flat pixel coordinates.
+
+
+	As such, they can't use the same cam_speed most of the time.
+	Zooming doesn't affect offset velocity, but it will affect target velocity.\
+
+	Zooming is centered on the target.
+	So, an offset of 0,0, placing the target at the upper left corner, will zoom in/out there.
+
+
+	*/
+
+	Camera2D camera;
+	Rectangle offset_bounds;
+	 
+	int target_speed_multiplier;
+	float target_speed;
+
+	float offset_speed = 5;
+	static constexpr int OFFSET_INCREMENT = 2;
+
+	// Need to recalculate the target speed on zoom in and out.
+	void recalculate_speed_target();
+
+public:
+
+	AdvCamera(Vector2& offset, Vector2& target);
+	AdvCamera(Vector2& offset, Vector2& target, int target_speed_multiplier);
+
+
+	AdvCamera(Vector2&& offset, Vector2&& target);
+	AdvCamera(Vector2&& offset, Vector2&& target, int target_speed_multiplier);
+
+	void move_target(Direction dir);
+	void move_offset(Direction dir);
+
+	void move_target(Vector2 vec);
+	void move_offset(Vector2 vec);
+
+	void set_target(Vector2 vec);
+	void set_offset(Vector2 vec);
+
+
+	void zoom_in();
+	void zoom_out();
+
+
+	// Increases the rate at which this camera will travel when move_target called with a direction.
+	void increase_speed_target();
+	// Decreases the rate at which this camera will travel when move_target called with a direction.
+	void decrease_speed_target();
+
+	// Increases the rate at which this camera will travel when move_offset called with a direction.
+	void increase_speed_offset();
+	// Decreases the rate at which this camera will travel when move_offset called with a direction.
+	void decrease_speed_offset();
+
+	void set_zoom(float level) { camera.zoom = level; }
+
+	float get_zoom() const { return camera.zoom; }
+
+
+	// Returns the underlying raylib Camera2D struct of this camera.
+	const Camera2D& get_raylib_camera() const { return camera; }
+
+};
+
