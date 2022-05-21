5d9528c874bbdf947557bac1ac5919e6e2a20233
darkmatterturtlepasta <95717261+Xxthedarkmatterturtlepasta003xX@users.noreply.github.com>
2022-05-20 19:13:40 -0700

    Added rem_body command to the spatial partitioning interface.

--- Planets2/SpatialPartitioning.h
+++ Planets2/SpatialPartitioning.h
@@ -10,6 +10,7 @@ class SpatialPartitioning
 public:
 
 	virtual void add_body(Body& body) = 0;
+	virtual void rem_body(const Body& body) = 0;
 	virtual void collision_check(std::vector<Body*>& to_remove) = 0;
 	virtual void update() = 0; // Update after moving all bodies.
 
