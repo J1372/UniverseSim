9fc0b9287218ff95c84f5e2b49e4d09e710af664
darkmatterturtlepasta <95717261+Xxthedarkmatterturtlepasta003xX@users.noreply.github.com>
2022-05-24 05:38:35 -0700

    Added general intersecting and containing methods in physics between a Body and a Rectangle to Physics namespace.

--- Planets2/Physics.cpp
+++ Planets2/Physics.cpp
@@ -25,3 +25,31 @@ void Physics::handle_collision(Body& body1, Body& body2)
 		on_collision_observers.notify_all(collision);
 	}
 }
+
+bool Physics::point_in_rect(Vector2 point, Rectangle rect)
+{
+	return point.x >= rect.x and point.x < rect.x + rect.width and
+		point.y >= rect.y and point.y < rect.y + rect.height;
+}
+
+bool Physics::body_inside_rect(const Body& body, Rectangle rect)
+{
+	return body.top().y >= rect.y and body.bottom().y <= rect.y + rect.height and
+		body.left().x >= rect.x and body.right().x <= rect.x + rect.width;
+}
+
+bool Physics::body_intersects_rect(const Body& body, Rectangle rect)
+{
+	float dist_x = std::abs(body.x - rect.x - rect.width / 2);
+	float dist_y = std::abs(body.y - rect.y - rect.width / 2);
+
+	if (dist_x > (rect.width / 2 + body.radius)) { return false; }
+	if (dist_y > (rect.height / 2 + body.radius)) { return false; }
+
+	if (dist_x <= (rect.width / 2)) { return true; }
+	if (dist_y <= (rect.height / 2)) { return true; }
+
+	int corner_dist = std::pow((dist_x - rect.width / 2), 2) + std::pow((dist_y - rect.height / 2), 2);
+
+	return corner_dist <= std::pow(body.radius, 2);
+}
--- Planets2/Physics.h
+++ Planets2/Physics.h
@@ -3,6 +3,8 @@
 
 class Body;
 struct Collision;
+struct Rectangle;
+struct Vector2;
 
 namespace Physics {
 
@@ -14,4 +16,13 @@ namespace Physics {
 
 	// Checks if two bodies have collided, and notifies all collision observers if they have.
 	void handle_collision(Body& body1, Body& body2);
+
+	// Returns true if a point is inside a rectangle.
+	bool point_in_rect(Vector2 point, Rectangle rect);
+
+	// Returns true if a body is completely inside a rectangle.
+	bool body_inside_rect(const Body& body, Rectangle rect);
+
+	// Returns true if a body intersects a rectangle.
+	bool body_intersects_rect(const Body& body, Rectangle rect);
 }
