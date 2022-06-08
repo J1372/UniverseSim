cc2cdf2e00003b7ddd0ca355b2727bb717d091f9
darkmatterturtlepasta <95717261+Xxthedarkmatterturtlepasta003xX@users.noreply.github.com>
2022-06-07 22:14:59 -0700

    Added methods for setting label text and color, and centering label on a coordinate.

--- Planets2/Label.cpp
+++ Planets2/Label.cpp
@@ -3,6 +3,28 @@
 Label::Label(const std::string& text, int x, int y, int font_size) : text(text), x(x), y(y), font_size(font_size)
 {}
 
+void Label::set_text(const std::string& to_set)
+{
+	text = to_set;
+}
+
+const std::string& Label::get_text() const
+{
+	return text;
+}
+
+void Label::set_color(Color to_set)
+{
+	color = to_set;
+}
+
+void Label::center_on(int center_x)
+{
+	int text_length = MeasureText(text.c_str(), font_size);
+
+	x = center_x - text_length / 2;
+}
+
 void Label::render() const
 {
 	DrawText(text.c_str(), x, y, font_size, color);
--- Planets2/Label.h
+++ Planets2/Label.h
@@ -3,19 +3,40 @@
 #include "raylib.h"
 #include <string>
 
+// A label to display basic standalone text with a font size and color in a gui.
 class Label : public UIElement
 {
-
+	// The text to be displayed.
 	std::string text;
+
+	// Starting position of the text.
+
 	int x;
 	int y;
+
+	// Font size of text
 	int font_size;
+
+	// Text's display color.
 	Color color = BLACK;
 
 public:
 
 	Label(const std::string& text, int x, int y, int font_size);
 
+	// Sets the label's text.
+	void set_text(const std::string& to_set);
+
+	// Gets the label's text.
+	const std::string& get_text() const;
+
+	// Sets color of the label's text.
+	void set_color(Color to_set);
+
+	// Moves the label's x-coordinate such that it is centered around the given x-coordinate.
+	void center_on(int center_x);
+
+	// Draws the label.
 	void render() const;
 };
 
