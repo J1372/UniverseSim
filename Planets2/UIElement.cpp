64084349bc43d13f5e72af153c7e451d0348ee6c
darkmatterturtlepasta <95717261+Xxthedarkmatterturtlepasta003xX@users.noreply.github.com>
2022-06-08 20:52:28 -0700

    Added a toggle hide command to UIElement as well as comments.

--- Planets2/UIElement.cpp
+++ Planets2/UIElement.cpp
@@ -1,4 +1,47 @@
 #include "UIElement.h"
 #include "raylib.h"
 
-bool UIElement::contains_point(Vector2 point) const { return false; }
+bool UIElement::contains_point(Vector2 point) const
+{
+	return false;
+}
+
+void UIElement::hide()
+{
+	hidden = true;
+}
+
+void UIElement::show()
+{
+	hidden = false;
+}
+
+void UIElement::toggle_hide()
+{
+	hidden = !hidden;
+}
+
+bool UIElement::is_hidden() const
+{
+	return hidden;
+}
+
+bool UIElement::is_visible() const
+{
+	return !hidden;
+}
+
+void UIElement::activate()
+{
+	active = true;
+}
+
+void UIElement::deactivate()
+{
+	active = false;
+}
+
+bool UIElement::send_keypress(int key_code)
+{
+	return false;
+}
--- Planets2/UIElement.h
+++ Planets2/UIElement.h
@@ -1,27 +1,54 @@
 #pragma once
 struct Vector2;
 
+// An element of a gui that may be shown, hid, or sent input to in order for it to react.
 class UIElement
 {
 protected:
 
+	// Whether the element should be rendered.
 	bool hidden = false;
-	bool active = false; // Some elements render differently when active.
+
+	// Whether the element is currently activated.
+	bool active = false;
 
 public:
 
-	void hide() { hidden = true; }
-	void show() { hidden = false; }
-	bool is_hidden() const { return hidden; }
+	// Marks the element so that it should not be rendered.
+	void hide();
 
-	void activate() { active = true; }
-	virtual void deactivate() { active = false; }
+	// Marks the element so that it should be rendered.
+	void show();
 
+	// Toggles whether the element is hidden.
+	void toggle_hide();
 
+	// Returns true if the element is hidden.
+	bool is_hidden() const;
+
+	// Returns true if the element is not hidden.
+	bool is_visible() const;
+
+	// Notifies the element that it has been activated.
+	void activate();
+
+	// Notifies the element that it has been deactivated.
+	virtual void deactivate();
+
+	// Notifies the element that it has been clicked on by the left mouse button.
 	virtual void click() {};
-	virtual bool send_keypress(int key_code) { return false; }
+
+	// Sends a key press to the element.
+	// Returns true if the element responded to the key, else false.
+	virtual bool send_keypress(int key_code);
+
+	// Notifies the element that the left mouse button is being dragged.
 	virtual void notify_drag() {};
+
+	// Returns true if the element contains the point, else false.
 	virtual bool contains_point(Vector2 point) const;
+
+	// Renders the element using screen coordinates.
 	virtual void render() const = 0;
 
 	virtual ~UIElement() = default;
