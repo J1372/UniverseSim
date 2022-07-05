e260865b4acac1d52d0f16b67eebd00105e5f2e6
J1372 <95717261+J1372@users.noreply.github.com>
2022-07-05 12:12:17 -0700

    Added additional comments to SpatialPartitioning.

--- Planets2/SpatialPartitioning.h
+++ Planets2/SpatialPartitioning.h
@@ -10,7 +10,8 @@ struct Collision;
 
 class DebugInfo;
 
-// An interface for spatial partitionings thatt can be used for collision detection.
+// An interface for spatial partitionings that can be used for collision detection.
+// Supports implementations that rebuild on tick as well as those that update theirselves.
 class SpatialPartitioning
 {
 protected:
@@ -19,6 +20,13 @@ protected:
 	int num_collision_checks_tick = 0;
 
 public:
+
+	// Implementations that rebuild on tick must still implement add_body, rem_body.
+	// If they do not, issues would occur when the simulation is paused.
+	// examples:
+	// 1. Representation not updating after creating a system until unpaused.
+	// 2. find_body will not work correctly until unpaused.
+	//		(user would be unable to attach camera to new body created while paused)
 
 	// Adds a body to be checked for collision.
 	virtual void add_body(Body& body) = 0;
